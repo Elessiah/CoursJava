7815a90d3af8a07614fdd3d69c3d9e8c54a5878f
Firekiking84 <kgalaxie84@gmail.com>
2025-12-11 16:44:08 +0100

    TP6bis terminé + ajout de Modifying

--- bestioles-jpa/bestioles/src/main/java/fr/m1sdv/bestioles/repositories/PersonRepository.java
+++ bestioles-jpa/bestioles/src/main/java/fr/m1sdv/bestioles/repositories/PersonRepository.java
@@ -3,6 +3,7 @@ package fr.m1sdv.bestioles.repositories;
 import fr.m1sdv.bestioles.model.Animal;
 import fr.m1sdv.bestioles.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,6 +13,7 @@ public interface PersonRepository extends JpaRepository<Person, Integer>, Person
     List<Person> findAllByFirstnameOrLastname(String firstname, String lastname);
     List<Person> findAllByAgeGreaterThanEqual(int age);
 
+    @Modifying
     void deleteAllByFirstname(String firstname);
 
     @Query("from Person WHERE age between :agemin and :agemax")
